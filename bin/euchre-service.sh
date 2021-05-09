84f0d7146b3f223390176883293b9cb250905854
Alex Karle <alex@alexkarle.com>
2021-05-09 00:36:49 -0400

    deploy: Modify rc.d service to only listen on localhost
    Previously, it was possible to hit http://euchre.live:3000 . I don't
    want to encourage HTTP usage, so this patch modifies the rc.d script to
    only listen on localhost:3000 so that external users cannot access via
    the plaintext port (must go through relayd on 443).

--- bin/euchre-service.sh
+++ bin/euchre-service.sh
@@ -5,7 +5,7 @@
 INSTALL="/home/_euchre/prod-el"
 # end config
 
-daemon="perl $INSTALL/gloat.pl daemon -m production"
+daemon="perl $INSTALL/gloat.pl daemon -m production -l http://127.0.0.1:3000"
 daemon_user="_euchre"
 
 . /etc/rc.d/rc.subr
